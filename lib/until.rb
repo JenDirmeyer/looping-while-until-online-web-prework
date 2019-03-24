c352d7b602682c4fb4f9a584aa692e979825a656
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-03-24 01:02:42 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,7 +2,7 @@ def using_until
         levitation_force = 6
         until levitation_force == 10 do
           "Wingardium Leviosa"
-          levitation_force = 0 
+          levitation_force += 1  
         end  
     
 end
